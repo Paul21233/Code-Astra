dcf1e5f5bfd8a4758e8695f1849d8e8ca9274ee9
Sowrin Paul <paul.sowrin2002@gmail.com>
2024-03-24 23:34:55 +0600

    changed some values-2

--- src/main/java/com/example/FinalProject/App.java
+++ src/main/java/com/example/FinalProject/App.java
@@ -1,4 +1,26 @@
 package com.example.FinalProject;
 
-public class App {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class App extends Application {
+    @Override
+    public void start(Stage stage) throws Exception{
+        try{
+            Parent root = FXMLLoader.load(App.class.getResource("Fxml/windowOne.fxml"));
+            Scene scene = new Scene(root);
+            stage.setScene(scene);
+            stage.show();
+        }
+        catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
 }
--- src/main/java/com/example/FinalProject/Controllers/HomeController.java
+++ src/main/java/com/example/FinalProject/Controllers/HomeController.java
@@ -1,4 +1,33 @@
 package com.example.FinalProject.Controllers;
 
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 public class HomeController {
+    private Stage stage;
+    private Scene scene;
+    private Parent root;
+
+    public void switchToScene1(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("Fxml/windowOne.fxml"));
+        stage = (Stage) ((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public void switchToScene2(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("Fxml/Register/register.fxml"));
+        stage = (Stage) ((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
 }
