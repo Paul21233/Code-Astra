bf4c9cf308460fe149509139ac081e3b63ba371e
Sowrin Paul <paul.sowrin2002@gmail.com>
2024-04-15 01:19:54 +0600

    login page change

--- src/main/java/com/example/FinalProject/Controllers/Login/LoginController.java
+++ src/main/java/com/example/FinalProject/Controllers/Login/LoginController.java
@@ -1,4 +1,21 @@
 package com.example.FinalProject.Controllers.Login;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
 public class LoginController {
+
+    @FXML
+    public Button loginbtn;
+
+    @FXML
+    public TextField usernamField;
+
+    @FXML
+    public PasswordField passField;
+
+    @FXML
+    public Button registerbtn;
 }
