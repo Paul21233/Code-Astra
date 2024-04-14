33f0735c28ab0b7757991d92a8b06e2f30f599fe
Sowrin Paul <paul.sowrin2002@gmail.com>
2024-04-15 01:10:13 +0600

    last changes

--- src/main/java/com/example/FinalProject/Controllers/HomeController.java
+++ src/main/java/com/example/FinalProject/Controllers/HomeController.java
@@ -1,34 +1,30 @@
 package com.example.FinalProject.Controllers;
 
-
-import javafx.event.ActionEvent;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Node;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
-import javafx.stage.Stage;
-
-import java.io.IOException;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 
 public class HomeController {
-    private Stage stage;
-    private Scene scene;
-    private Parent root;
 
-//    public void switchToScene1(ActionEvent event) throws IOException {
-//        Parent root = FXMLLoader.load(getClass().getResource("Fxml/windowOne.fxml"));
-//        stage = (Stage) ((Node)event.getSource()).getScene().getWindow();
-//        scene = new Scene(root);
-//        stage.setScene(scene);
-//        stage.show();
-//    }
-//
-//    public void switchToScene2(ActionEvent event) throws IOException {
-//        Parent root = FXMLLoader.load(getClass().getResource("Fxml/register.fxml"));
-//        stage = (Stage) ((Node)event.getSource()).getScene().getWindow();
-//        scene = new Scene(root);
-//        stage.setScene(scene);
-//        stage.show();
-//    }
+    @FXML
+    public TextField searchbar;
 
+    @FXML
+    public Label homebtn;
+
+    @FXML
+    public Label editbtn;
+
+    @FXML
+    public Label aboutbtn;
+
+    @FXML
+    public Label loginbtn;
+
+    @FXML
+    public Button beginbtn;
+
+    @FXML
+    public Button interbtn;
 }
