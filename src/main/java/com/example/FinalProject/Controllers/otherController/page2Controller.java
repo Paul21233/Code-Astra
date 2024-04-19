776788956939e28a69af989eb30ffc70eefdc1d8
Sowrin Paul <paul.sowrin2002@gmail.com>
2024-04-19 23:36:16 +0600

    page2 added

---
+++ src/main/java/com/example/FinalProject/Controllers/otherController/page2Controller.java
@@ -0,0 +1,39 @@
+package com.example.FinalProject.Controllers.otherController;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+import javafx.scene.image.ImageView;
+
+public class page2Controller {
+
+    @FXML
+    public Button cbtn;
+
+    @FXML
+    public Button htmlbtn;
+
+    @FXML
+    public Button pythonbtn;
+
+    @FXML
+    public Button cppbtn;
+
+    @FXML
+    public Button javabtn;
+
+    @FXML
+    public Button cssbtn;
+
+    @FXML
+    public ImageView profiePic;
+
+    @FXML
+    public TextArea showIcon;
+
+    @FXML
+    public TextArea overview;
+
+    @FXML
+    public Button nextbtn;
+}
